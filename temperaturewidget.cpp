7a1707e2c84b82a4962f1b8406136aa305a04174
ZH <zhanghan@gmx.cn>
2017-02-10 18:15:33 +0800

    Added TemperatureWidget for temperature and humidity reading
    Should refactor widgets for reading and plot

--- temperaturewidget.cpp
+++ temperaturewidget.cpp
@@ -1,6 +1,35 @@
 #include "temperaturewidget.h"
+#include "datasource.h"
+
+#include <QLabel>
+#include <QFormLayout>
 
 TemperatureWidget::TemperatureWidget(QWidget *parent) : QWidget(parent)
 {
+    QFormLayout *layout = new QFormLayout(this);
 
+    QLabel *temperatureTextLabel = new QLabel ("Temperature", this);
+    QLabel *temperatureValueLabel = new QLabel (this);
+
+    QLabel *humidityTextLabel = new QLabel ("Humidity", this);
+    QLabel *humidityValueLabel = new QLabel (this);
+
+    layout->addRow(temperatureTextLabel, temperatureValueLabel);
+    layout->addRow(humidityTextLabel, humidityValueLabel);
+
+    DataSource &dataSource = DataSource::instance();
+
+    connect(&dataSource, &DataSource::valueChanged, [=] (DataSource::DataType type, float value) {
+        switch (type)
+        {
+        case DataSource::Temperature:
+            temperatureValueLabel->setText(QString::number(value));
+            break;
+        case DataSource::Humidity:
+            humidityValueLabel->setText(QString::number(value));
+            break;
+        default:
+            break;
+        }
+    });
 }
