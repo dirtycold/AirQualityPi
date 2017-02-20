88dc8b1e5e6e74d1a86fd89d24846bdf19768e2e
ZH <zhanghan@gmx.cn>
2017-02-20 15:00:33 +0800

    Added unified initialization for DataRefiner and DataSource

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "airqualitypiwidget.h"
 #include <QApplication>
 #include "datasource.h"
+#include "datarefiner.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    DataSource &dataSource = DataSource::instance();
+    DataRefiner &dataRefiner = DataRefiner::instance();
+    dataRefiner.setSource(&dataSource);
+
     AirQualityPiWidget w;
     w.show();
 
